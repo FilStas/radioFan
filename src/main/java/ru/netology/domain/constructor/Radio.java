31b064a1fe3d49a2a30daccb0cc335addae4ee8f
FilStas <filstas23@gmail.com>
2020-06-06 17:20:09 +0400

    added new files project

---
+++ src/main/java/ru/netology/domain/constructor/Radio.java
@@ -0,0 +1,75 @@
+package ru.netology.domain.constructor;
+
+public class Radio {
+    private int currentStationNumber;
+    private int maxStationNumber = 9;
+    private int minStationNumber = 0;
+    private int currentSoundVolume;
+    private int maxSoundVolume = 10;
+    private int minSoundVolume = 0;
+
+    public void addStationNumber() {
+        if (currentStationNumber == maxStationNumber) {
+            currentStationNumber = minStationNumber;
+            return;
+        }
+        currentStationNumber++;
+    }
+
+    public void downStationNumber() {
+        if (currentStationNumber == minStationNumber) {
+            currentStationNumber = maxStationNumber;
+            return;
+        }
+        currentStationNumber--;
+
+    }
+
+    public void addSoundVolume() {
+        if (currentSoundVolume == maxSoundVolume) {
+            return;
+        }
+        currentSoundVolume++;
+
+
+    }
+
+    public void downSoundVolume() {
+        if (currentSoundVolume == minSoundVolume) {
+            return;
+        }
+        currentSoundVolume--;
+    }
+
+
+    public int getCurrentStationNumber() {
+        return currentStationNumber;
+    }
+
+    public void setCurrentStationNumber(int currentStationNumber) {
+        if (currentStationNumber > maxStationNumber) {
+            return;
+        }
+        if (currentStationNumber < minStationNumber) {
+            return;
+        }
+        this.currentStationNumber = currentStationNumber;
+    }
+
+    public int getCurrentSoundVolume() {
+        return currentSoundVolume;
+    }
+
+    public void setCurrentSoundVolume(int currentSoundVolume) {
+        if (currentSoundVolume > maxSoundVolume) {
+            return;
+
+        }
+        if (currentSoundVolume < minSoundVolume) {
+            return;
+
+        }
+        this.currentSoundVolume = currentSoundVolume;
+    }
+
+}
